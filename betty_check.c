3c6beefde16a177f4a92da3c11a94f962604079f
Edward Ebenezer <alabishg@gmail.com>
2023-05-19 01:42:21 -0700

    Check betty

---
+++ betty_check.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * mult_shell - multiplies two integer
+ * @x: First integer
+ * @y: Second integer
+ *
+ * Return: Value of the integers.
+ */
+int mult_shell(int x, int y)
+{
+	return (x * y);
+}
---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fctnl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int mult_shell(int x, int y);
+
+#endif
